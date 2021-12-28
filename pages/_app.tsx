331daf024b97d643d1abd2fd6e9175bdeb6e3f54
Sean Morgan <seantbmorgan@gmail.com>
2021-12-28 16:28:30 -0500

    created next/ts project and added in axe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
+  const ReactDOM = require("react-dom");
+  const axe = require("@axe-core/react");
+  axe(React, ReactDOM, 1000);
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
