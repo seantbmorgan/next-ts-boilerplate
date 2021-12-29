ea2190956b6b763d530af6874ec9b43046825b56
Sean Morgan <seantbmorgan@gmail.com>
2021-12-28 21:28:10 -0500

    axios and swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app';
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
   const ReactDOM = require('react-dom'); // eslint-disable-line global-require, @typescript-eslint/no-var-requires
   const axe = require('@axe-core/react'); // eslint-disable-line import/no-extraneous-dependencies, global-require, @typescript-eslint/no-var-requires
-  // const config = {} // https://github.com/dequelabs/axe-core/blob/master/doc/API.md#api-name-axeconfigure
+  // const config = {} // https://github.com/dequelabs/axe-core-npm/tree/develop/packages/react
   axe(React, ReactDOM, 1000);
 }
 
